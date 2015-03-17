2bee823494fba68d041040aa0d4ba3ad7927b4ed
Christopher Pedersen <chrisaped@gmail.com>
2015-03-17 08:09:05 -0400

    made more changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,27 +13,23 @@ class User < ActiveRecord::Base
     REFERRAL_STEPS = [
         {
             'count' => 5,
-            "html" => "Shave<br>Cream",
-            "class" => "two",
-            "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png")
+            "html" => "Clip/Trailer<br>5 minutes",
+            "class" => "two"
         },
         {
             'count' => 10,
-            "html" => "Truman Handle<br>w/ Blade",
-            "class" => "three",
-            "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png")
+            "html" => "TV Episode<br>5 minutes",
+            "class" => "three"
         },
         {
             'count' => 25,
-            "html" => "Winston<br>Shave Set",
-            "class" => "four",
-            "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png")
+            "html" => "Short Film",
+            "class" => "four"
         },
         {
             'count' => 50,
-            "html" => "One Year<br>Free Blades",
-            "class" => "five",
-            "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png")
+            "html" => "Feature Film",
+            "class" => "five"
         }
     ]
 
