32b1729a879d13f43e43828ae5bd32ae39591697
Christopher Pedersen <chrisaped@gmail.com>
2015-03-29 13:59:02 -0400

    mail preview fix

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,9 +1,11 @@
-class MailPreview < MailView
+if Rails.env.development?
+  class MailPreview < MailView
 
-  def signup_email
-    user = Struct.new(:email, :referral_code).new('name@example.com', 'f19ad2ad3b')
-    mail = UserMailer.signup_email(user)
-    mail
+    def signup_email
+      user = Struct.new(:email, :referral_code).new('name@example.com', 'f19ad2ad3b')
+      mail = UserMailer.signup_email(user)
+      mail
+    end
+
   end
-
 end
